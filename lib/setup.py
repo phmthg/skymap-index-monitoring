61a9fbf866444956e5a8357ecfe212efe4e92c33
thong.pd <thongktqt10@gmai.com>
2024-03-18 17:39:38 +0700

    f

--- lib/setup.py
+++ lib/setup.py
@@ -2,15 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='skymap_index_monitoring',
-    version='0.1.0',
-    url='http://git.skymapglobal.vn/monitor-services/docker-service/skymap-index-monitoring.git',
+    version='0.1',
+    packages=find_packages(),
+    description='A sample Python project',
+    author='Sky',
+    author_email='thongktqt10@email.com',
+    url='https://github.com/phmthg/skymap-index-monitoring',
     packages=find_packages(),
     install_requires=[
-        # "requests",
         "flask",
         "rasterio",
         "geopandas",
-        "rio-tiler>=4,<5",
-        ""
+        "rio-tiler>=4,<5"
     ],
 )
