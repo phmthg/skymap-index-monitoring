81cb07a62e5b8b596b0acf6f365c42594df2473a
thong.pd <thongktqt10@gmai.com>
2024-03-18 17:48:25 +0700

    init

--- setup.py
+++ setup.py
@@ -3,12 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='skymap_index_monitoring',
     version='0.1',
-    packages=find_packages(),
     description='A sample Python project',
     author='Sky',
     author_email='thongktqt10@email.com',
     url='https://github.com/phmthg/skymap-index-monitoring',
-    packages=find_packages(),
     install_requires=[
         "flask",
         "rasterio",
